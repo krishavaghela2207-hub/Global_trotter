7c6da5b16d561f9dcde7b94f11356f92b98cafba
PatelJinsi <pateljinu2104@gmail.com>
2026-08-22 16:21:06 +0530

    Delete __init__ (1).py

--- __init__ (1).py
+++
@@ -1,8 +0,0 @@
-"""
-ML Model Package for Travel Recommendation System
-Handles caching of trained models with pickle for faster loading
-"""
-
-from .recommend import recommend_places, regenerate_cache
-
-__all__ = ['recommend_places', 'regenerate_cache']
