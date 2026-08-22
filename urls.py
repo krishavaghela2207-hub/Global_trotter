851d9f2c6f8436dcaa7c89193ffb2f1c29e7a4b5
PatelJinsi <pateljinu2104@gmail.com>
2026-08-22 15:58:04 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,14 +1,39 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for travel_prject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+
+from django.urls import path, include
+
+from django.conf import settings
+
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('destination/<str:destination_name>/', views.destination_detail, name='destination_detail'),
-    path('search/', views.search_results, name='search'),
-    path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
-    path('add-favorite/<str:destination_name>/', views.add_favorite_csv, name='add_favorite'),
-    path('favorites/', views.favorite_page, name='favorites'),
-    path('remove-favorite/<int:id>/', views.remove_favorite, name='remove_favorite'),
+
+    path('admin/', admin.site.urls),
+
+    path('', include('destinations.urls')),
+
 ]
+
+if settings.DEBUG:
+
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
